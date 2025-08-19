4436254eb8c0b742e21154562d70679e74b61c36
Logeshwaran-U <logeshwaranuk@gmail.com>
2025-08-19 12:28:25 +0530

    Assignment01 and Assignmen02 completed

---
+++ Assignment02/src/com/linkesh/assignment02/Student.java
@@ -0,0 +1,16 @@
+package com.linkesh.assignment02;
+
+
+
+public class Student {
+    
+    // Default Constructor
+    Student() {
+        System.out.println("Student object is created");
+    }
+
+    public static void main(String[] args) {
+        // When object is created, constructor runs automatically
+        Student s1 = new Student();
+    }
+}
